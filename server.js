5246430737dcab78ef86a0ee253ef3a3f09bfeed
CyberDildonix <RoksonixProd@gmail.com>
2018-04-21 11:10:49 +0300

    Test

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname + '/build'));
+// app.use(express.static(__dirname + '/build'));
 app.listen(process.env.PORT || 8080);
 app.get('/*all', function(req, res) {
     res.sendFile(path.join(__dirname + '/build/index.html'));
