c998747288bd212a124b434a86a210e9b4d13abc
CyberDildonix <RoksonixProd@gmail.com>
2018-04-21 12:01:29 +0300

    Heroku

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 const express = require('express');
-const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname + '/build'));
 app.listen(process.env.PORT || 8080);
 app.get('/', function(req, res) {
-    res.sendFile('/build/index.html');
+    res.sendFile('/index.html');
 });
